d2d8a9a3e28b38c78d6c456023a8ddba48632a83
Federico Torres <federicoivantorres1@gmail.com>
2024-02-25 20:48:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,109 @@
+import csv
+import os
+import shutil
+
+import matplotlib.pyplot as plt
+import requests
+
+# Constants
+medicamento1 = os.getenv('med1')
+medicamento2 = os.getenv('med2')
+output_dir = "./output"
+data_dir = "./data"
+
+
+def download_csv(filename, file_url):
+
+  # Create the output directory if it doesn't exist
+  os.makedirs(data_dir, exist_ok=True)
+
+  # Send a GET request to the file URL
+  dosis = requests.get(
+      f'https://drive.google.com/uc?id={os.getenv(file_url)}&export=download')
+
+  # Check if the request was successful
+  if dosis.status_code == 200:
+    # Save the content of the response (file) to a local file in the output directory
+    file_path = os.path.join(data_dir, filename)
+    with open(file_path, 'wb') as f:
+      f.write(dosis.content)
+    print(f"File {filename} downloaded successfully to the 'output' folder.")
+  else:
+    print(f"Failed to download {filename}.")
+
+
+def procesar_medicamento(filepath):
+
+  # Listas para almacenar los datos
+  fechas = []
+  dosis = []
+
+  # Leer el archivo CSV
+  with open(filepath, 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+      fecha = f"{row['Mes']} {row['Dia']}"  # Concatenamos mes y día
+      fechas.append(fecha)
+      dosis.append(float(row['Dosis']))
+
+  return fechas, dosis
+
+
+# Set filenames
+med1_file = f'dosis_{medicamento1}.csv'
+med2_file = f'dosis_{medicamento2}.csv'
+
+# Descargar archivos
+download_csv(med1_file, medicamento1)
+download_csv(med2_file, medicamento2)
+
+# Procesar datos
+fecha1, dosis1 = procesar_medicamento(f'./{data_dir}/{med1_file}')
+fecha2, dosis2 = procesar_medicamento(f'./{data_dir}/{med2_file}')
+
+# Crear el gráfico
+fig, ax1 = plt.subplots(figsize=(10, 6))  # Tamaño de la figura
+ax2 = ax1.twinx()  # Crear un segundo eje y
+
+# Plotear los datos del medicamento 1 en el primer eje y (izquierda)
+ax1.plot(fecha1,
+         dosis1,
+         marker='',
+         linestyle='-',
+         color='blue',
+         label=medicamento1)
+ax1.set_ylabel(f'Dosis de {medicamento1} [mg]', color='blue')
+
+# Plotear los datos del medicamento 2 en el segundo eje y (derecha)
+ax2.plot(fecha2,
+         dosis2,
+         marker='',
+         linestyle='--',
+         color='purple',
+         label=medicamento2)
+ax2.set_ylabel(f'Dosis de {medicamento2} [mg]', color='purple')
+
+# Añadir etiquetas y título
+plt.title('Dosis de Medicamentos de Octubre a Marzo')
+plt.xlabel('Fecha [Mes]')
+
+# Añadir leyendas
+lines1, labels1 = ax1.get_legend_handles_labels()
+lines2, labels2 = ax2.get_legend_handles_labels()
+ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper left')
+
+n = 7
+ax1.set_xticks(range(0, len(fecha1), n))  # Ajustar las etiquetas del eje x
+ax1.set_xticklabels(fecha1[::n], rotation=45)
+
+# Mostrar la gráfica
+plt.grid(True)  # Habilitar cuadrícula
+plt.tight_layout()  # Ajustar diseño
+plt.savefig("./output/plot.png")  # Guardar la gráfica en un archivo
+
+try:
+  # Remove the folder and its contents
+  shutil.rmtree('data')
+  print("Folder ./data and its contents removed successfully.")
+except Exception as e:
+  print(f"An error occurred while removing folder ./data: {e}")
