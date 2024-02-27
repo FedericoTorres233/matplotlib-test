b56e29d01f190c8df4f5b0d3822c02682c269fe9
Federico Torres <federicoivantorres1@gmail.com>
2024-02-27 21:36:07 +0000

    refactor: translate everything to english

--- main.py
+++ main.py
@@ -6,47 +6,66 @@ import matplotlib.pyplot as plt
 import requests
 
 # Constants
-medicamento1 = os.getenv('MED1')
-medicamento2 = os.getenv('MED2')
+med1 = os.getenv('MED1')
+med2 = os.getenv('MED2')
 output_dir = "./output"
 data_dir = "./data"
+plot_filename = "plot"
 
 
-def download_csv(filename, file_url):
+def download_csv(filename, file_key):
+  """
+  This function downloads a file from google drive
+
+  Parameters:
+  filename (string): Name of the file to be stored locally.
+  file_key (string): Name of the key whose value is a link to the .csv file
+  """
 
   # Create the output directory if it doesn't exist
   os.makedirs(data_dir, exist_ok=True)
 
   # Send a GET request to the file URL
-  dosis = requests.get(
-      f'https://drive.google.com/uc?id={os.getenv(file_url)}&export=download')
+  file = requests.get(
+      f'https://drive.google.com/uc?id={os.getenv(file_key)}&export=download')
 
   # Check if the request was successful
-  if dosis.status_code == 200:
-    # Save the content of the response (file) to a local file in the output directory
+  if file.status_code == 200:
+    # Save the content of the response to a local file in the data directory
     file_path = os.path.join(data_dir, filename)
     with open(file_path, 'wb') as f:
-      f.write(dosis.content)
+      f.write(file.content)
     print(f"File {filename} downloaded successfully to the 'output' folder.")
   else:
     print(f"Failed to download {filename}.")
 
 
-def procesar_medicamento(filepath):
+def process_medicine(filepath):
+  """
+  This function processes the .csv files downloaded
 
-  # Listas para almacenar los datos
-  fechas = []
-  dosis = []
+  Parameters:
+  filename (string): Path of the .csv file to be processed.
 
-  # Leer el archivo CSV
+  Returns:
+  array[string]: The area of the circle.
+  array[float]: The area of the circle.
+  """
+
+  # Initial arrays
+  dates = []
+  dose = []
+
+  # Read csv files
   with open(filepath, 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
-      fecha = f"{row['Mes']} {row['Dia']}"  # Concatenamos mes y día
-      fechas.append(fecha)
-      dosis.append(float(row['Dosis']))
+      date = f"{row['Mes']} {row['Dia']}"  # Concatenate month and day
+      dates.append(date)
+      dose.append(float(row['Dosis']))
 
-  return fechas, dosis
+  # Return arrays with csv data
+  return dates, dose
 
 
 # Remove downloaded files
@@ -59,63 +78,67 @@ def remove_folder(folder):
     print(f"An error occurred while removing folder {folder}: {e}")
 
 
-def plot_data(fecha1, dosis1, fecha2, dosis2):
-  # Crear el gráfico
-  fig, ax1 = plt.subplots(figsize=(10, 6))  # Tamaño de la figura
-  ax2 = ax1.twinx()  # Crear un segundo eje y
+def plot_data(dates1, doses1, dates2, doses2):
+  """
+  This function plots the data using matplotlib and stores the graph locally.
 
-  # Plotear los datos del medicamento 1 en el primer eje y (izquierda)
-  ax1.plot(fecha1,
-           dosis1,
-           marker='',
-           linestyle='-',
-           color='blue',
-           label=medicamento1)
-  ax1.set_ylabel(f'Dosis de {medicamento1} [mg]', color='blue')
+  Parameters:
+  dates1 (array[float]): Array that contains all dates for medicine 1
+  doses1 (array[string]): Array that contains the doses for medicine 1 ordered by date
+  dates2 (array[float]): Array that contains all dates for medicine 2
+  doses1 (array[string]): Array that contains the doses for medicine 2 ordered by date
+  """
 
-  # Plotear los datos del medicamento 2 en el segundo eje y (derecha)
-  ax2.plot(fecha2,
-           dosis2,
+  fig, ax1 = plt.subplots(figsize=(10, 6))  # Set fig size
+  ax2 = ax1.twinx()  # Create a second y-axis
+
+  # Plot the data for medicine 1 on the first y-axis (left)
+  ax1.plot(dates1, doses1, marker='', linestyle='-', color='blue', label=med1)
+  ax1.set_ylabel(f'Dosis de {med1} [mg]', color='blue')
+
+  # Plot the data for drug 2 on the second y-axis (right)
+  ax2.plot(dates2,
+           doses2,
            marker='',
            linestyle='--',
            color='purple',
-           label=medicamento2)
-  ax2.set_ylabel(f'Dosis de {medicamento2} [mg]', color='purple')
+           label=med2)
+  ax2.set_ylabel(f'Dosis de {med2} [mg]', color='purple')
 
-  # Añadir etiquetas y título
+  # Add labels and title
   plt.title('Dosis de Medicamentos de Octubre a Marzo')
   plt.xlabel('Fecha [Mes]')
 
-  # Añadir leyendas
+  # Add legend
   lines1, labels1 = ax1.get_legend_handles_labels()
   lines2, labels2 = ax2.get_legend_handles_labels()
   ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper left')
 
-  # Ajustar las etiquetas del eje x
+  # Adjust x-axis labels
   n = 7
-  ax1.set_xticks(range(0, len(fecha1), n))
-  ax1.set_xticklabels(fecha1[::n], rotation=45)
+  ax1.set_xticks(range(0, len(dates1), n))
+  ax1.set_xticklabels(dates1[::n], rotation=45)
 
-  # Mostrar la gráfica
-  plt.grid(True)  # Habilitar cuadrícula
-  plt.tight_layout()  # Ajustar diseño
-  plt.savefig("./output/plot.png")  # Guardar la gráfica en un archivo
+  # Show the graph
+  plt.grid(True)  # Enable grid
+  plt.tight_layout()  # Adjust layout
+  plt.savefig(f"./output/{plot_filename}.png")  # Save graph
 
 
 # Set filenames
-med1_file = f'dosis_{medicamento1}.csv'
-med2_file = f'dosis_{medicamento2}.csv'
+med1_file_local = f'doses_{med1}.csv'
+med2_file_local = f'doses_{med2}.csv'
 
-# Descargar archivos
-download_csv(med1_file, medicamento1)
-download_csv(med2_file, medicamento2)
+# Download files
+download_csv(med1_file_local, med1)
+download_csv(med2_file_local, med2)
 
-# Procesar datos
-fecha1, dosis1 = procesar_medicamento(f'./{data_dir}/{med1_file}')
-fecha2, dosis2 = procesar_medicamento(f'./{data_dir}/{med2_file}')
+# Process data from csv files
+dates1, doses1 = process_medicine(f'./{data_dir}/{med1_file_local}')
+dates2, doses2 = process_medicine(f'./{data_dir}/{med2_file_local}')
 
-# Plot data
-plot_data(fecha1, dosis1, fecha2, dosis2)
+# Plot all data
+plot_data(dates1, doses1, dates2, doses2)
 
-# Remove folder
+# Remove data folder
 remove_folder(data_dir)
